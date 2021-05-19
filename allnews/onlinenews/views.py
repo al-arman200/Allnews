691e81c005877a77af92a6eddc17f78ad9e42a93
al-arman200@gmail <al.arman200@gmail.com>
2021-05-19 17:17:36 +0430

    firs view for setup app

--- allnews/onlinenews/views.py
+++ allnews/onlinenews/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http      import HttpResponse
 
+
+def index(request):
+    return HttpResponse("firs step for all news")
 # Create your views here.
+
+
