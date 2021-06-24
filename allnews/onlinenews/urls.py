f77444a1165a42713d39222b47812cf2c77b3b6e
Arman <al.arman200@gmai.com>
2021-06-24 16:48:21 +0430

    fix static file for show img shamsipor

--- allnews/onlinenews/urls.py
+++ allnews/onlinenews/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.urls import path , include
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     #path('' , views.index , name='index') ,   
     path('' , views.home , name='home')
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
